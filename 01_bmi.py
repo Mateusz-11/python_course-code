b970517c6301a64528c443d24698878d3ee7eedf
Mateusz-11 <tweak.night@gmail.com>
2024-05-22 14:35:40 +0200

    Tasks 01-03

---
+++ 01_bmi.py
@@ -0,0 +1,6 @@
+height = float(input("Write your height (m)"))
+weight = int(input("Write your weight (kg)"))
+
+bmi = weight/height**2
+
+print(f'Your BMI is equal: {bmi}')
---
+++ 03_trapeze_area.py
@@ -0,0 +1,7 @@
+base1 = float(input('Text base first: '))
+base2 = float(input('Text base second: '))
+height = float(input('Text height: '))
+
+area = ((base1+base2) * height) / 2
+
+print(f'Area of this trapeze is: {area}')
