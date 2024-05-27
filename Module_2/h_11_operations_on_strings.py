a43b8ad23fd996e689649187ca5af040af472993
Mateusz-11 <107146883+Mateusz-11@users.noreply.github.com>
2024-05-27 20:26:48 +0200

    Update h_11_operations_on_strings.py
    Add comment

--- Module_2/h_11_operations_on_strings.py
+++ Module_2/h_11_operations_on_strings.py
@@ -1,6 +1,9 @@
+# Create loop which output contains words with the same first and last letter.
+
 sentence = "Litwo ojczyzno moja Ty jesteś jak zdrowie"
 
 for word in sentence.split():
     if word[0].lower() == word[-1].lower():
         print(word)
 
+# result: ojczyzno
