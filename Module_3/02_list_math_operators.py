ca69451ae671413cffa92fcd0cf2b7765f7f24ae
Mateusz-11 <tweak.night@gmail.com>
2024-05-30 21:53:14 +0200

    Resolve 2 task of lists - math operators

---
+++ Module_3/02_list_math_operators.py
@@ -0,0 +1,14 @@
+list_of_numbers = []
+
+for i in range(1, 6):
+    new_number = int(input(f'Write number {i} of 5: '))
+    list_of_numbers.append(new_number)
+
+min_value = min(list_of_numbers)
+max_value = max(list_of_numbers)
+avg_value = sum(list_of_numbers) / len(list_of_numbers)
+
+print(f'All list: {list_of_numbers}')
+print(f'Minimum value: {min_value}')
+print(f'Maximum value: {max_value}')
+print(f'Average value: {avg_value}')
