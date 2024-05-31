fc583846b20108b1f3661e681174af49f7838cde
Mateusz-11 <tweak.night@gmail.com>
2024-05-31 12:03:02 +0200

    Refactor of task 1

--- Module_3/04_dict_translate.py
+++ Module_3/04_dict_translate.py
@@ -1,6 +1,12 @@
 eng_pol_dict = {'house': 'dom', 'tree': 'drzewo', 'phone': 'telefon', 'city': 'miasto'}
 direct_translate = input('Which language you want translate? eng / pl: ')
 
+# veryfication of choice
+if direct_translate not in ('eng', 'pl'):
+    print('Bad answer! Run program once again.')
+    quit()
+
+# translation word
 if direct_translate == 'eng':
     word_to_translate = input('What word do you want translate?: ')
     print(eng_pol_dict.get(word_to_translate))
