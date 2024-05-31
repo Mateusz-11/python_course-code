1ec07e1325ce708684966f23259aedb3dbc06256
Mateusz-11 <tweak.night@gmail.com>
2024-05-31 17:10:37 +0200

    Refactor of task 2

--- Module_3/05_dict_creating_new_dict.py
+++ Module_3/05_dict_creating_new_dict.py
@@ -2,14 +2,17 @@ text = 'raz raz dwa trzy trzy trzy raz'
 
 list_words = text.split()
 print(list_words)
-
 counter_words = {}
 
+# 1 version
+# for word in list_words:
+#     if word not in counter_words.keys():
+#         counter_words[word] = 1
+#     else:
+#         counter_words.update({word: counter_words[word] + 1})
+
+## 2 version
 for word in list_words:
-    if word not in counter_words.keys():
-        counter_words[word] = 1
-    else:
-        print('update value')
-        counter_words.update({word: counter_words[word] + 1})
+    counter_words[word] = counter_words.get(word, 0) + 1
 
 print(counter_words)
