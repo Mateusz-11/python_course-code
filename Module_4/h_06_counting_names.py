d404d9a5564d5ee5ebaca404114ad2d3e3245375
Mateusz-11 <tweak.night@gmail.com>
2024-06-19 16:52:19 +0200

    Solution task - task 6 - with while loop

---
+++ Module_4/h_06_counting_names.py
@@ -0,0 +1,13 @@
+result = {}
+while True:
+    status = input('If you finished, write "End". To continue press Enter: ')
+    if status.lower() == "end":
+        break
+    print('- - - ' * 5)
+    name = input('Write name to add: ')
+    # print(result.get(name, 0) + 1)
+    result[name] = result.get(name, 0) + 1
+    print(result)
+
+names = sorted(list(result.keys()))
+# print(names)
