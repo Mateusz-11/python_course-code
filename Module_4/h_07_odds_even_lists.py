f1e923f85baf5a26703b97054bd4f1383eace6f1
Mateusz-11 <107146883+Mateusz-11@users.noreply.github.com>
2024-07-02 14:13:55 +0200

    Update h_07_odds_even_lists.py

--- Module_4/h_07_odds_even_lists.py
+++ Module_4/h_07_odds_even_lists.py
@@ -12,10 +12,10 @@ while True:
     else:
         odds.append(number)
 
-print('- - - ' * 5)
+print('- - - ' * 7)
 print(f'Odds numbers: {odds}')
 print(f'Numbers of odds numbers: {len(odds)}')
-print('- - - ' * 5)
-print('- - - ' * 5)
+print('- - - ' * 7)
+print('- - - ' * 7)
 print(f'Evens numbers: {even}')
 print(f'Numbers of evens numbers: {len(even)}')
