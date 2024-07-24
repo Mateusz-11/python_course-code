8b3d806535221c705c7675600a375cd465f69801
Mateusz-11 <tweak.night@gmail.com>
2024-07-24 17:17:56 +0200

    Init exercise 8

---
+++ Module_4/h_08_.py
@@ -0,0 +1,11 @@
+products = {
+    'doniczka': 15,
+    'konewka': 20,
+    'grabie': 12,
+    'szpadel': 18,
+    'kosiarka': 350,
+    'łopata': 22,
+    'siekiera': 28,
+    'młotek': 12,
+    'wiadro': 8,
+}
