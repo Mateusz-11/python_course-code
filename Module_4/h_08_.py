583aa6bbb30b3ea23be68e006ab570593483c604
Mateusz-11 <tweak.night@gmail.com>
2024-07-27 14:40:01 +0200

    Update task 8

--- Module_4/h_08_.py
+++ Module_4/h_08_.py
@@ -15,24 +15,24 @@ for name, price in products.items():
 print('- - - ' * 5)
 
 cart = {}
-
+basket_sum = 0
 while True:
     print('Your cart:')
-    basket_sum = 0
+
     for name, quantity in cart.items():
         print(f"- {name} - {quantity}")
-        basket_sum += 4
 
     print('- - - ' * 4)
-    print(f'Cart value: {basket_sum}')
+    print(f'Cart value: {basket_sum} zł')
     print('- - - ' * 4)
 
     status = input('If you want to finish, write "End". To continue press Enter: ')
     if status.lower() == "end":
+        print(f'Cart value: {basket_sum} zł')
         break
     print('- - - ' * 4)
     choice = input('What you want to buy? ')
     cart[choice] = cart.get(choice,0) + int(input('How many the product you want to buy? '))
-
+    basket_sum += cart[choice] * products[choice]
 
 
