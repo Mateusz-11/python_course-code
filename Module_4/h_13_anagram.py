053e77b966173c95bc4f360a93b6ccc29bfbe0ef
Mateusz-11 <tweak.night@gmail.com>
2024-08-01 18:40:51 +0200

    Update

--- Module_4/h_13_anagram.py
+++ Module_4/h_13_anagram.py
@@ -4,8 +4,8 @@ word2 = "fgahjskldaccab"
 list1 = list(word1)
 list2 = list(word2)
 
-# print(sorted(list1))
-# print(sorted(list2))
+print(sorted(list1))
+print(sorted(list2))
 
 if sorted(list1) == sorted(list2):
     print("These words are anagrams")
