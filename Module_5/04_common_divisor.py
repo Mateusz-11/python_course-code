8c39af0fb22445413640733bd3337e5805eb206e
Mateusz-11 <tweak.night@gmail.com>
2024-08-03 19:17:51 +0200

    Solutions with defaults arguments in functions

---
+++ Module_5/04_common_divisor.py
@@ -0,0 +1,15 @@
+def calculate_common_divisor(num1, num2, start=2):
+    set1 = set()
+    set2 = set()
+    for i in range(start, num1+1):
+        if num1 % i == 0:
+            set1.add(i)
+    for j in range(start, num2+1):
+        if num2 % j == 0:
+            set2.add(j)
+    return set2 & set1
+
+
+print(calculate_common_divisor(3,6))
+print(calculate_common_divisor(3,6, 4))
+print(calculate_common_divisor(16,8))
---
+++ Module_5/05_defult_argument.py
@@ -0,0 +1,6 @@
+def multiply(num1, num2=2):
+    return num1 * num2
+
+
+print(multiply(2))
+print(multiply(2, 3))
