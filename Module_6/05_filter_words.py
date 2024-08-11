489a3d55e2fc8e6a71956b0d31ecf0f23f70166b
Mateusz-11 <tweak.night@gmail.com>
2024-08-11 14:56:33 +0200

    Solution task 5

--- Module_6/05_filter_words.py
+++ Module_6/05_filter_words.py
@@ -1,9 +1,13 @@
-def filater_words(text: str) -> set:
-    pass
+def filter_words(text: str) -> set:
+    result = set()
+    for word in text.split(' '):
+        if word[0].lower() not in 'aeiouyąę' and word[-1].lower() in 'aeiouyąę':
+            result.add(word)
 
+    return result
 
 def test_filter_words():
-    assert filater_words("Ala ma kota") == {'ma', 'kota'}
-    assert filater_words("Ala MA KOTA") == {'MA', 'KOTA'}
-    assert filater_words("Ala Ma kotA") == {'Ma', 'kotA'}
-    assert filater_words("tygrys") == set()
+    assert filter_words("Ala ma kota") == {'ma', 'kota'}
+    assert filter_words("Ala MA KOTA") == {'MA', 'KOTA'}
+    assert filter_words("Ala Ma kotA") == {'Ma', 'kotA'}
+    assert filter_words("tygrys") == set()
