2ed3dbf5ec7191e01a8b3590d2c2a583e711f0d1
Mateusz-11 <tweak.night@gmail.com>
2024-08-16 13:15:10 +0200

    Solution task 9

---
+++ Module_7/09_tkinter_02.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import messagebox
+from random import randint
+
+def on_submit():
+    global previous_difference
+    value = int(field.get())
+    difference = abs(target - value)
+
+    if difference == 0:
+        messagebox.showinfo('Condition', "Congratulation!")
+    elif previous_difference is None or difference < previous_difference:
+        messagebox.showinfo('Condition', "Getting warmer")
+    else:
+        messagebox.showinfo('Condition', "Getting colder")
+
+    previous_difference = difference
+
+previous_difference = None
+target = randint(1,100)
+
+window = tk.Tk()
+window.title('Guess number')
+
+label = tk.Label(window, text='Write number')
+label.pack()
+
+field = tk.Entry()
+field.pack()
+
+button = tk.Button(text="Guess!", command=on_submit)
+button.pack()
+
+window.mainloop()
