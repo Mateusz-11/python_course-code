df8e3a298b222c82dad0237bd31ffea4816077e3
Mateusz-11 <tweak.night@gmail.com>
2024-08-19 20:46:12 +0200

    Solution task 13

--- Module_7/13_operations_on_txt_file.py
+++ Module_7/13_operations_on_txt_file.py
@@ -1,3 +1,13 @@
-with open('13_students_data.txt') as file:
+passed = []
+
+with open('13_students_data.txt', encoding='utf8') as file:
     for line in file:
-        print(line)
+        first_name, last_name, note = line.strip().split(';')
+        if note == '2':
+            passed.append({'first_name': first_name, 'last_name': last_name, 'note': note})
+
+with open('13_output.txt', encoding='utf8', mode='w') as file:
+    for student in passed:
+        file.write(f'{student.get("first_name")};{student.get("last_name")},{student.get("note")}\n')
+
+# print(passed)
