9f273ab8df4f8c19fbe14421276e8edaff47210f
Mateusz-11 <tweak.night@gmail.com>
2024-08-30 20:45:20 +0200

    Part of task 15 - JSON - printing of json data

--- Module_7/15_json_task_1.py
+++ Module_7/15_json_task_1.py
@@ -1,5 +1,18 @@
+from json import load
+
 new_author = 'Witold Gombrowicz'
 new_title = "Ferdydurke"
 
 with open('15_books.json') as file:
-    print(file.read())
+    # it generate string
+    # print(file.read())
+
+    # it generate list
+    # print(load(file))
+
+    books = load(file)
+
+for book in books:
+    print("Title: ", book.get('title'))
+    print("Author: ", book.get('author'))
+    print("- - - " * 5)
