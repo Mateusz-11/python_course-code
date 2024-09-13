22715c8f7069598029a2ddf53f004424715f723b
Mateusz-11 <tweak.night@gmail.com>
2024-09-13 18:28:42 +0200

    Initial task 5

--- Module_8/05_sqlite_introduction.py
+++ Module_8/05_sqlite_introduction.py
@@ -1,2 +1,27 @@
-import csv
+import sqlite3
+
+## option 1
+# connection = sqlite3.connect('library.db')
+# connection.close()
+
+## option 2 - context manager
+# with sqlite3.connect('library.db') as connection:
+#     ...
+
+## select in db
+with sqlite3.connect('library.db') as connection:
+    cursor = connection.cursor()
+    books = cursor.execute('SELECT book_id, title, author FROM books')
+
+    for book in books:
+        print(book)
+
+## adding values
+with sqlite3.connect('library.db') as connection:
+    cursor = connection.cursor()
+    title = input('Tytuł: ')
+    author = input('Autor: ')
+    cursor.execute('INSERT INTO books VALUES (null, ?, ?)', (title, author))
+    connection.commit()
+
 
