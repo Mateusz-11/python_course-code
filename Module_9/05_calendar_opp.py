7ee617a0aa02d2a93f77408a612f4af27d1682cb
Mateusz-11 <tweak.night@gmail.com>
2024-09-23 19:11:25 +0200

    Init task 5 OPP calendar

--- Module_9/05_calendar_opp.py
+++ Module_9/05_calendar_opp.py
@@ -2,8 +2,20 @@ from datetime import datetime
 
 
 class Meeting:
-    def __init__(self, title: str, date_of_meeting: datetime, room: str):
+    def __init__(self, date_of_meeting: datetime, title: str):
         self.title = title
         self.date_of_meeting = date_of_meeting
-        self.room = room
+
+class Calendar:
+    def __init__(self):
+        self.meetings = {}
+
+    def show_meetings(self):
+        return 1
+
+    def check_the_date(self):
+        return 1
+
+
+
 
